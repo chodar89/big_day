9e3a6486766becdeab3c502e5cf63bfbf930f272
Tobiasz Chodarewicz <43481390+chodar89@users.noreply.github.com>
2020-03-06 14:25:17 +0000

    Add static files and set up static root directory in settings plus add template folder with base.html

--- big_day/settings.py
+++ big_day/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = {
+    os.path.join(BASE_DIR, 'big_day/static')
+}
